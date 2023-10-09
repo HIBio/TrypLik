ea38e44d9450f1aacb770e385b895e50e37e426a
Jonathan Carroll <github@jcarroll.com.au>
2023-10-09 10:30:10 +1030

    genomes data and vignette updates

---
+++ data-raw/genomes.R
@@ -0,0 +1,13 @@
+## code to prepare `genomes` dataset goes here
+
+genomes <- read.csv(system.file("extdata", "1KGP_EUR_figure_raw.csv", package = "TrypLik"))
+colnames(genomes) <- c("2A/D", "2B/D", "Geno Group")
+lvls <- c(
+  "0α:4β" = 0,
+  "1α:3β" = 1,
+  "2α:2β" = 2,
+  "everything else" = 3
+)
+genomes$`Geno Group` <- names(lvls[match(genomes$`Geno Group`, lvls)])
+
+usethis::use_data(genomes, overwrite = TRUE)
